7ea9f4ed816ee4b63a5445ea458ff5ed53064c99
Andrii <Krenevych>
2021-02-20 12:15:26 +0200

    Додано тестові дані до лабораторної роботи 3.

--- labs/lab01/L1/data_helper/input_file_generator.py
+++ labs/lab01/L1/data_helper/input_file_generator.py
@@ -1,13 +1,14 @@
 from random import randint
 
-N_MAXKEY = 100
+N_MAXKEY = 25
+MULT = 4
 
 def generate(fname, equations_number):
     with open(fname, "w", encoding='utf-8') as f_out:
         for i in range(equations_number):
-            coef2 = randint(-N_MAXKEY, N_MAXKEY)
-            coef1 = randint(-N_MAXKEY, N_MAXKEY)
-            coef0 = randint(-N_MAXKEY, N_MAXKEY)
+            coef2 = randint(0, MULT) * randint(-N_MAXKEY, N_MAXKEY)
+            coef1 = randint(0, MULT) * randint(-N_MAXKEY, N_MAXKEY)
+            coef0 = randint(0, MULT) * randint(-N_MAXKEY, N_MAXKEY)
             print("%4d %4d %4d" % (coef2, coef1, coef0), file=f_out)
 
         print(file=f_out)
